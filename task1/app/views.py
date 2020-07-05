8136bfa298f38f0b771bd9c36bd95a9ff05aec03
Smikhalcv <Smikhalcv@yandex.ru>
2020-07-06 00:20:18 +0300

    Minifix

--- task1/app/views.py
+++ task1/app/views.py
@@ -2,11 +2,11 @@ import csv
 
 from django.shortcuts import render
 
+
 def inflation_view(request):
     template_name = 'inflation.html'
 
     inf_rus_list = []
-    inf_rus_dict = {}
 
     with open('inflation_russia.csv') as file:
         info = csv.DictReader(file, delimiter=';')
@@ -20,18 +20,9 @@ def inflation_view(request):
                 else:
                     line[key] = int(value)
             inf_rus_list.append(line)
-    # print(inf_rus_list)
-    #
-    # for i in inf_rus_list:
-    #     for key, value in i.items():
-    #         if key in inf_rus_dict.keys():
-    #             inf_rus_dict[key].append(value)
-    #         else:
-    #             inf_rus_dict[key] = [value]
 
     # чтение csv-файла и заполнение контекста
     context = {
-        #'info_dict': inf_rus_dict,
         'head': list(inf_rus_list[0].keys()),
         'info_list': inf_rus_list
     }
