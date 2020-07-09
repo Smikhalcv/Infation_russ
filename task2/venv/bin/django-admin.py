ce99dc21273c75a0011600f310bc56a048589f86
Smikhalcv <Smikhalcv@yandex.ru>
2020-07-10 00:24:08 +0300

    Task 2

---
+++ task2/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/dell-ubuntu/7084BDD384BD9C521/dj/Динамическое формирование страниц/task2/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
